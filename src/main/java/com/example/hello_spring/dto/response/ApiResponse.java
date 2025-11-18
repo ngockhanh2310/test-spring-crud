560827678bbccab8085e76a3ecf453b4494cf152
Nguyen Ngoc Khanh <nguyenngockhanh231003@gmail.com>
2025-11-18 09:23:52 +0700

    api response

--- src/main/java/com/example/hello_spring/dto/response/ApiResponse.java
+++ src/main/java/com/example/hello_spring/dto/response/ApiResponse.java
@@ -2,25 +2,11 @@ package com.example.hello_spring.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ApiResponse<T> {
-    private String message;
-    private T data;
-
-    public ApiResponse(String message) {
-        this.message = message;
-    }
-
-    public ApiResponse(String message, T data) {
-        this.message = message;
-        this.data = data;
-    }
+public record ApiResponse<T>(
+        String message,
+        T data
+) {
 }
